c5bfa59d4e2d56270768666dc5375171258dac55
Neelu1 <neelkamalsemwal@gmail.com>
2017-12-19 23:00:10 +0530

    EVM files in assembly

---
+++ memory.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+    FILE *p;
+    int i=0,flag=0;
+    p=fopen("TEST2_mem","r");
+    char c,ar[]="201008040201000002";
+    char ar2[18];
+  while(((c=fgetc(p))!=EOF)&&(i!=18))
+  {
+    ar2[i]=c;
+    i++;
+  }
+   fclose(p);
+    for(i=0;i<18;i++)
+    {
+     printf("%c",ar2[i]);
+    }
+    for(i=0;i<18;i++)
+    {
+     if(ar[i]!=ar2[i])
+      flag=1;
+    }
+    if(flag==0)
+        printf("the code is correct");
+    else
+        printf("the code is not correct");
+}
